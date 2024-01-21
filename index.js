0977dc4596d64e3ec603381b19f16995cf4cbdda
Tiago Costa <tcperez@cea.plm.pt>
2024-01-21 14:45:44 +0000

    adicionado rotaVideo ao backend

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const mensagemRoute = require('./src/routes/mensagem');
 const videoRoute = require('./src/routes/videos');
+const playVideo = require('./src/routes/playVideosRoute');
 
 const app = express();
 
 // Aplica o CORS globalmente para todas as rotas
 app.use(cors());
 
-// Use a rota específica
+// Use a rota específicax
 app.use('/api/mensagem', mensagemRoute);
 app.use('/api/videos', videoRoute);
+app.use('/api/play-video', playVideo);
 
 const PORT = 3001;
 app.listen(PORT, () => {
